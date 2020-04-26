a96c26383913e7aae3571aacd8462665f75c82ad
nuno chev <nunochev@live.com>
2020-04-26 17:21:12 +0200

    Estilo Padrao

--- src/componentes/Simples.js
+++ src/componentes/Simples.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import Padrao from '../estilo/Padrao'
 //Componente baseado em funcao
 
+export default props =>
+    <Text style={[Padrao.ex]}>Arrow 1 : {props.texto}</Text>
+
+
+//Exemplo 1
 {/*export default function(props) {
     return <Text>{props.texto}</Text>
 }*/}
 
-
+//Exemplo 2
+{/*}
 export default  props =>
 <View>
     <Text>Arrow 1: {props.texto}</Text>
     <Text>Arrow 2: {props.texto}</Text>
 </View>
+*/}
