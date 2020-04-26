e4609dd075efb2cf5d51ac91c925e02b667a7d77
nuno chev <nunochev@live.com>
2020-04-26 16:45:27 +0200

    Componente funcional

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import Simples from './componentes/Simples'
+
+//Componete Baseado em Classe
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.f20}>App!</Text>
+		<Simples/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex :1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	f20: {
+		fontSize: 20
+	}
+})
---
+++ src/componentes/Simples.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+//Componente baseado em funcao
+
+export default function() {
+    return <Text>Simples ! </Text>
+}
