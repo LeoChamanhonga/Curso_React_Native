6da4be191ce744860737943c40df2eccdb504cdc
nuno chev <nunochev@live.com>
2020-04-26 17:41:27 +0200

    Renderizacao Condicional

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import Simples from './componentes/Simples'
-
+import ParImpar from './componentes/ParImpar'
 //Componete Baseado em Classe
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.f20}>App!</Text>
-		<Simples texto = 'Flexivel' />
+		<Simples texto="Flexivel" />
+		<ParImpar numero={30}/>
+		
       </View>
     );
   }
---
+++ src/componentes/ParImpar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import Padrao from '../estilo/Padrao'
+
+//Renderizacao condicional
+
+export default props =>
+<View>
+    {
+        props.numero % 2 == 0
+        ? <Text style={Padrao.ex}>Par</Text>
+        : <Text style={Padrao.ex}>Impar</Text>
+    }
+</View>
