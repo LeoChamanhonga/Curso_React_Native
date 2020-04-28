fcccfa5eea298ca8440ebdd123e9ccca37ef9509
nuno chev <nunochev@live.com>
2020-04-28 15:51:04 +0200

    Componente Filho

--- src/componentes/ComunicacaoDirecta.js
+++ src/componentes/ComunicacaoDirecta.js
@@ -1,2 +1,9 @@
 import React from 'react'
 import { View, Text} from 'react-native'
+
+const fonte = { style: { fontSize: 30 }}
+
+export const Filho = props =>
+  <View>
+      <Text {...fonte}>Filho: {props.nome} {props.sobrenome}</Text>
+  </View>
