0918e20a720a5882f29d2ac318920d26460c0a98
nuno chev <nunochev@live.com>
2020-04-28 10:49:36 +0200

    Cuidado com 'this'
    Se o 'this' nao for usado em uma funcao q nao eh Arrow, ele ira variar de acordo com a chamada

--- src/componentes/Contador.js
+++ src/componentes/Contador.js
@@ -6,14 +6,16 @@ import {View, Text, TouchableHighlight} from 'react-native';
 export default class Contador extends Component{
    //Estados
     state = {
-        numero: 0
+        numero: this.props.numeroInicial
+        //numero: 0
     }
-    maisUm = () => {
+    maisUm() {
         this.setState({ numero: this.state.numero + 1})
     }
 
     limpar = () => {
-        this.setState({ numero: 0})
+        this.setState({ numero: this.props.numeroInicial})
+        //this.setState({ numero: 0})
     }
     
     render(){
@@ -22,7 +24,8 @@ export default class Contador extends Component{
             <View>
                 <Text style={{fontSize: 40}}>{this.state.numero}</Text>
                 <TouchableHighlight
-                  onPress={this.maisUm}
+                  //Passar a funcao do metodo this
+                  onPress={() => this.maisUm()}
                   onLongPress={this.limpar}>
                   <Text>Incrementar/Zerar</Text> 
                 </TouchableHighlight>
