e9716082e640533e9fc190c36544e9e544d8cec6
nuno chev <nunochev@live.com>
2020-04-28 11:29:27 +0200

    Update Contador.js
    O "O prop no STATE" faz com q o menu nao aparece!! Atencao a isso

--- src/componentes/Contador.js
+++ src/componentes/Contador.js
@@ -6,16 +6,16 @@ import {View, Text, TouchableHighlight} from 'react-native';
 export default class Contador extends Component{
    //Estados
     state = {
-        numero: this.props.numeroInicial
-        //numero: 0
+        //numero: this.props.numeroInicial
+        numero: 0
     }
     maisUm() {
         this.setState({ numero: this.state.numero + 1})
     }
 
     limpar = () => {
-        this.setState({ numero: this.props.numeroInicial})
-        //this.setState({ numero: 0})
+        //this.setState({ numero: this.props.numeroInicial})
+        this.setState({ numero: 0})
     }
     
     render(){
