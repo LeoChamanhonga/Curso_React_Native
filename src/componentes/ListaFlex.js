7b44653d7e867332b9c3c789179c34593dea8691
nuno chev <nunochev@live.com>
2020-05-07 10:41:27 +0200

    Lista de alunos
    1 Array contendo 1 aluno por Id

---
+++ src/componentes/ListaFlex.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ScrollView, View, FlatList, Text } from 'react-native'
+
+//Lista de Alunos
+const alunos = [
+    {id: 1, nome: 'Joao', nota: 7.9},
+    {id: 2, nome: 'Araujo', nota: 8.9},
+    {id: 3, nome: 'Rafaela', nota: 7.9},
+    {id: 4, nome: 'Shelton', nota: 6.9},
+    {id: 5, nome: 'Cristina', nota: 5.9},
+    {id: 6, nome: 'Asley', nota: 4.9},
+    {id: 7, nome: 'Chanaya', nota: 3.9},
+    {id: 8, nome: 'Rodney', nota: 10.9},
+    {id: 9, nome: 'Quentin', nota: 6.2},
+
+]
