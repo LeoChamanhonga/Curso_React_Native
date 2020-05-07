16bd41ca11755417d58b5d4d5b79e6bd126dfa30
nuno chev <nunochev@live.com>
2020-05-07 09:34:05 +0200

    Funcao p gerir a a Props Filhos
    Forma dinamica de optimizar as propriedades em uma funcao

--- src/componentes/ComunicacaoDirecta.js
+++ src/componentes/ComunicacaoDirecta.js
@@ -3,6 +3,11 @@ import { View, Text} from 'react-native'
 
 const fonte = { style: { fontSize: 30 }}
 
+function filhosComprops(props){
+    {React.Children.map (props.children,
+        c => React.cloneElement(c, { ...props, ...c.props }))}
+}
+
 export const Filho = props =>
   <View>
       <Text {...fonte}>Filho: {props.nome} {props.sobrenome}</Text>
@@ -12,8 +17,8 @@ export const Pai = props =>
    <View>
        <Text {...fonte}>Pai: {props.nome} {props.sobrenome}</Text>
        {/*{props.children}*/}
-       {React.Children.map (props.children,
-        c => React.cloneElement(c, { ...props, ...c.props }))}
+       {filhosComprops(props)}
+       
    </View>
 
 export const Avo = props =>
